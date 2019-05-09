03f27bfa6f0ef8c29d130ac9c75ecf742bc4e5f0
Vibhav Churi <churi.vibhav@hisabsoftware.co.in>
2019-05-09 20:48:22 +0530

    Refactored Extension Methods

--- Vhc.Optimus.ReferenceImpl/ServiceExtensions.cs
+++ Vhc.Optimus.ReferenceImpl/ServiceExtensions.cs
@@ -22,39 +22,13 @@ namespace Vhc.Optimus.ReferenceImpl
 
         public static IServiceCollection AddReferenceServices(this IServiceCollection services, IConfiguration config)
         {
-            services.AddTransient<INotificationService, ConsoleNotificationService>();
-            services.AddSingleton<IExecutionContext, ContinuousExecutionContext>();
-            services.AddTransient<ITextFileProvider, PlainTextFileProvider>();
-            services.AddJobLoader(unitFactory);
-            services.AddSingleton<IConnectionProvider, SqliteConnectionProvider>();
-            services.AddJobRunner(options => options.AddSqliteDatabase(config));
             System.Net.ServicePointManager.DefaultConnectionLimit = 50;
-            return services;
-        }
-
-        private static void AddJobLoader(this IServiceCollection services, Func<string, IJobUnit> unitFactoryFunction)
-        {
-            services.AddTransient<IJobLoader, JobLoader>(provider => new JobLoader(
-                      provider.GetService<IConfiguration>(),
-                      provider.GetService<ITextFileProvider>(),
-                      provider.GetService<ILogger<JobLoader>>(),
-                      unitFactoryFunction));
-        }
-
-        public static IServiceCollection AddJobRunner(this IServiceCollection services, Action<JobRunnerOptions> optionsModifier)
-        {
-            services.AddTransient<JobRunner>(provider =>
-            {
-                var jobRunner = new JobRunner(
-                    provider.GetService<IConnectionProvider>(),
-                    provider.GetService<ILogger<JobRunner>>(),
-                    provider.GetService<IJobLoader>(),
-                    provider.GetService<INotificationService>(),
-                    provider.GetService<IExecutionContext>()
-                    );
-                return jobRunner.WithOptions(optionsModifier);
-            });
-            return services;
+            return services.AddTransient<INotificationService, ConsoleNotificationService>()
+            .AddSingleton<IExecutionContext, ContinuousExecutionContext>()
+            .AddTransient<ITextFileProvider, PlainTextFileProvider>()
+            .AddJobLoader(unitFactory)
+            .AddSingleton<IConnectionProvider, SqliteConnectionProvider>()
+            .AddJobRunner(options => options.AddSqliteDatabase(config));
         }
 
         public static void AddSqliteDatabase(this JobRunnerOptions options, IConfiguration config)
---
+++ Vhc.Optimus/Services/OptimusExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using Vhc.Optimus.Core.Abstractions;
+using Vhc.Optimus.Core.Services;
+
+namespace Vhc.Optimus.Services
+{
+    public static class OptimusExtensions
+    {
+        public static IServiceCollection AddJobLoader(this IServiceCollection services, Func<string, IJobUnit> unitFactoryFunction) 
+            => services.AddTransient<IJobLoader, JobLoader>(provider => new JobLoader(
+                provider.GetService<IConfiguration>(),
+                provider.GetService<ITextFileProvider>(),
+                provider.GetService<ILogger<JobLoader>>(),
+                unitFactoryFunction));
+
+        public static IServiceCollection AddJobRunner(this IServiceCollection services, Action<JobRunnerOptions> optionsModifier) 
+            => services.AddTransient(provider =>
+            {
+                var jobRunner = new JobRunner(
+                    provider.GetService<IConnectionProvider>(),
+                    provider.GetService<ILogger<JobRunner>>(),
+                    provider.GetService<IJobLoader>(),
+                    provider.GetService<INotificationService>(),
+                    provider.GetService<IExecutionContext>()
+                    );
+                return jobRunner.WithOptions(optionsModifier);
+            });
+    }
+}
